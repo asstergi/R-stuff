201d5c22ec68e63130aad49f57ef261df271e9c0
asstergi <a.stergioudis@gmail.com>
2017-08-29 13:14:53 +0300

    Lookup table

--- R/predict.R
+++ R/predict.R
@@ -10,11 +10,15 @@ pred <- function(url) {
   #print (url)
   out <- tryCatch(
     {
-      markovchain::predict(model$estimate, newdata = url)
+      #markovchain::predict(model$estimate, newdata = url)
+      mcu[mcu$Row.Labels == url,]$pred
       #class(model$estimate)[1]
     },
     error=function(cond) {
       NA
     })
+  if (identical(out, character(0))){
+    out <- NA
+  }
   return (out)
 }
